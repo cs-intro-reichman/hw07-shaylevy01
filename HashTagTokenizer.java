a7fe594e764905ab11d65c64c266f645560aff01
Shay Levy <shay.levy01@post.runi.ac.il>
2024-02-02 11:26:48 +0200

    Commit1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,30 +11,40 @@ public class HashTagTokenizer {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		// This function read a local file of words, and store them in an array
+		for (int i=0 ; i<dictionary.length ; i++){
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		// This function take a string as an input and determine its presence in the dictionary
+		boolean result = false;
+		for (int i=0 ; i<dictionary.length ; i++){
+			if (dictionary[i].equals(word)){
+				result = true;
+			}
+		}
+		return result;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
- 
-        int N = hashtag.length();
-
+		hashtag = hashtag.toLowerCase();
+		int N = hashtag.length();
         for (int i = 1; i <= N; i++) {
-		
+			String prifix = hashtag.substring(0,i);
+			boolean isInDirectory = existInDictionary(prifix, dictionary);
+			if (isInDirectory == true){
+				System.out.println(prifix);
+				breakHashTag(hashtag.substring(i, N), dictionary);
+				return;
+			}
         }
     }
-
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -3,33 +3,62 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word = args[0];
-		int threshold = Integer.parseInt(args[1]);
+		String word = "coooool";
+		int threshold = 3;
+		//String word = args[0];
+		//int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		// This function gets a string and return its tail
+		return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		// This function implements the Levenshtein distance function
+		if (word2.isEmpty()) {
+			return word1.length();
+		}
+		if (word1.isEmpty()) {
+			return word2.length();
+		}
+		if (word1.charAt(0) == word2.charAt(0)){
+			return levenshtein(tail(word1), tail(word2));
+		} else {
+			int delete = levenshtein(tail(word1), word2) + 1;
+			int insert = levenshtein(word1, tail(word2)) + 1;
+			int replace = levenshtein(tail(word1), tail(word2)) + 1;	
+			return Math.min(delete,Math.min(insert, replace));
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		// This function read a local file of words, and store them in an array
+		for (int i=0 ; i<dictionary.length ; i++){
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		String result = word;
+		int minDistance = 300;
+		for (int i=0 ; i<dictionary.length ; i++){
+			int distance = levenshtein(word, dictionary[i]);
+			if (distance <= threshold){
+				if (distance < minDistance){
+					minDistance = Math.min(minDistance, distance);
+					result = dictionary[i];
+				}
+			}
+		} 
+		return result;
 	}
 
 }
